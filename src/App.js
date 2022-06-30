0c49e99d441d639dd46b3fc880288307fe1cc118
Leegukhyoun <dbkim512@naver.com>
2022-06-30 17:03:42 +0900

    styledcomponent-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import styled from 'styled-components';
+import Dialog from './components/Dialog';
+import { useState } from 'react';
+import Box from './components/Box';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 50px;
+  border: 1px solid black;
+  padding: 10px;
+`;
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  }
+  const onCancle = () => {
+    setDialog(false);
+    console.log('취소');
+  }
+  const onConfirm = () => {
+    setDialog(false);
+    console.log('삭제');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBlock>
+        <Button>BUTTON1</Button>
+        <Button>BUTTON2</Button>
+        <Button onClick={onClick}>BUTTON3</Button>
+      </AppBlock>
+    <Dialog title="정말로 삭제하시겠습니까?" confirmText="삭제" cancleText="취소" visible={dialog} onCancle={onCancle} onConfirm={onConfirm}>
+      데이터를 삭제하시겠습니까?
+    </Dialog>
+    <Box/>
+    </>
+
   );
 }
 
---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Box.module.css'
+
+const Box = (props) => {
+    return (
+        <div className={styles.Box}>나는 퐉스입니다.
+        </div>
+    );
+};
+
+export default Box;
